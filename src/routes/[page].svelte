17ea3fa4af786633d4f89c0bf91dc377e02c0165
pngwn <hello@pngwn.io>
2019-06-12 23:25:56 +0100

    Cleanup. Paginate page zero to index.

--- src/routes/[page].svelte
+++ src/routes/[page].svelte
@@ -6,21 +6,4 @@
 
 <script>
 	import Index from './_Index.html';
-	// import { onMount } from 'svelte'; 
-	// import MainView from './_components/MainView/index.html';
-	// import Tags from './_components/Tags.html';
-	// import * as api from './_api.js';
-
-	// let tab, tag, tags;
-
-	// function setTags({ detail }) {
-	// 	tag = detail.tag; 
-	// 	tab = "tag";
-	// }
-
-	// onMount(() => {
-	// 	api.get('tags').then((response) => {
-	// 		tags = response.tags
-	// 	});
-	// })
 </script>
