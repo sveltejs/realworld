a9cb4bae1872297b85ca81607d5ae2eb0575dc8c
Rich Harris <richard.a.harris@gmail.com>
2021-04-17 16:13:33 -0400

    bundle cookie/marked

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,8 +1,18 @@
 const vercel = require('@sveltejs/adapter-vercel');
+const pkg = require('./package.json');
 
 module.exports = {
 	kit: {
 		adapter: vercel(),
-		target: '#svelte'
+		target: '#svelte',
+		vite: {
+			ssr: {
+				// TODO the adapter should make this unnecessary
+				noExternal: [
+					...Object.keys(pkg.dependencies),
+					...Object.keys(pkg.devDependencies)
+				]
+			}
+		}
 	}
 };
