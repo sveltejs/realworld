3e09a5af70bb3a49a7c990e00ca3af1c31c60e01
Ben McCann <322311+benmccann@users.noreply.github.com>
2021-05-18 21:44:46 -0700

    Update file missed in earlier SvelteKit upgrade

--- src/routes/profile/@[user]/index.json.js
+++ src/routes/profile/@[user]/index.json.js
@@ -1,7 +1,7 @@
 import * as api from '$lib/api.js';
 
-export async function get({ params, context }) {
-	const { profile } = await api.get(`profiles/${params.user}`, context.user && context.user.token);
+export async function get({ params, locals }) {
+	const { profile } = await api.get(`profiles/${params.user}`, locals.user && locals.user.token);
 
 	return {
 		body: profile
