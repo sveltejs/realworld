54ed1f77b985a8489b293078545dc66ad89fa951
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-03-02 09:39:22 -0800

    only parse body if required

--- src/routes/auth/save.js
+++ src/routes/auth/save.js
@@ -2,14 +2,13 @@ import * as api from '$lib/api.js';
 import { respond } from './_respond';
 
 export async function post({ request, locals }) {
-	const user = await request.json();
-
 	if (!locals.user) {
 		return {
 			status: 401
 		};
 	}
 
+	const user = await request.json();
 	const { token } = locals.user;
 	const body = await api.put(
 		'user',
