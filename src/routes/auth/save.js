3ccafed87aff8c7f50fabf90ff0a72b159d5f3ea
BliiTzZ <yaya.ponsot@Gmail.com>
2022-03-02 18:39:50 +0100

    Fix save.js (#108)

--- src/routes/auth/save.js
+++ src/routes/auth/save.js
@@ -1,13 +1,14 @@
 import * as api from '$lib/api.js';
 import { respond } from './_respond';
 
-export async function post({ body: user, locals }) {
+export async function post({ request, locals }) {
 	if (!locals.user) {
 		return {
 			status: 401
 		};
 	}
 
+	const user = await request.json();
 	const { token } = locals.user;
 	const body = await api.put(
 		'user',
