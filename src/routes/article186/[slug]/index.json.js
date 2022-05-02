b9628e5bc5214fbc5c901c7e68fb272213a861fb
Axel <67882025+Axeldeblen@users.noreply.github.com>
2022-05-02 14:04:34 +1000

    clone article folder2000 times failing locally - memory heap limit

---
+++ src/routes/article186/[slug]/index.json.js
@@ -0,0 +1,14 @@
+import * as api from '$lib/api.js';
+
+export async function get({ params, locals }) {
+	const { slug } = params;
+	const { article } = await api.get(`articles/${slug}`, locals.user && locals.user.token);
+
+	return {
+		body: article
+	};
+}
+
+export async function put({ request }) {
+	console.log('put', request);
+}
