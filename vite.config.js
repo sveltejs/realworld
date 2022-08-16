4f57fe4757f060c4d3ee78ef0bae575a444b4890
Axel <67882025+Axeldeblen@users.noreply.github.com>
2022-08-17 09:47:01 +1000

    try limit the max file ops

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { sveltekit } from "@sveltejs/kit/vite";
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  rollupOptions: {
+    maxParallelFileOps: 5,
+  },
 };
 
 export default config;
