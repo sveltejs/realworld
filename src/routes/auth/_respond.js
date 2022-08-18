26d44c943b43d7e72e4e9ed786268a72f157f8cd
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-08-18 08:41:49 -0700

    import json function

--- src/routes/auth/_respond.js
+++ src/routes/auth/_respond.js
@@ -1,12 +1,11 @@
-import { error } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 
 export function respond(body) {
 	if (body.errors) {
 		throw error(401, body);
 	}
 
-	const json = JSON.stringify(body.user);
-	const value = Buffer.from(json).toString('base64');
+	const value = Buffer.from(JSON.stringify(body.user)).toString('base64');
 
 	return json(body, {
 		headers: {
