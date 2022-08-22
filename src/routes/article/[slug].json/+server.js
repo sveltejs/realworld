02d0a0713aa6de94f4fef1634e96e8dd7633eb4e
Pepe GC <jpgarcia90@gmail.com>
2022-08-22 16:05:39 -0400

    Implement article creation (#129)
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- src/routes/article/[slug].json/+server.js
+++ src/routes/article/[slug].json/+server.js
@@ -1,4 +1,4 @@
-import { json } from '@sveltejs/kit';
+import { error, json } from '@sveltejs/kit';
 import * as api from '$lib/api.js';
 
 export async function GET({ params, locals }) {
@@ -8,6 +8,26 @@ export async function GET({ params, locals }) {
 	return json(article);
 }
 
-export async function PUT({ request }) {
-	console.log('put', request);
+export async function PUT({ params, request, locals }) {
+	const form = await request.formData();
+	const slug = params.slug ;
+
+	const res = await api.put(
+		`articles/${slug}`,
+		{
+			article: {
+				title: form.get('title'),
+				description: form.get('description'),
+				body: form.get('body'),
+				tagList: form.get('tagList').split(/[\t\n, ]+/)
+			}
+		},
+		locals.user && locals.user.token
+	);
+
+	if (res?.errors) {
+		throw error(400, res.errors);
+	}
+
+	return json(res.article);
 }
