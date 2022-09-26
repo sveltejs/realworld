7766ffacb17ff3759981992f758c1b4b7b8cb41f
The Cashew Trader <81344401+thecashewtrader@users.noreply.github.com>
2022-09-26 14:01:39 +0530

    Rebase formatting changes
    Co-authored-by: Ben McCann <322311+benmccann@users.noreply.github.com>

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,11 +1,11 @@
 export async function post(endpoint, data) {
 	const response = await fetch(endpoint, {
-        method: 'POST',
-        credentials: 'include',
-        body: JSON.stringify(data || {}),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
-    return await response.json();
+		method: 'POST',
+		credentials: 'include',
+		body: JSON.stringify(data || {}),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	return await response.json();
 }
