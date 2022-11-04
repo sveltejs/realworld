58cfb55e25a76718f01744a1b2d7fb4489101d6a
Rich Harris <hello@rich-harris.dev>
2022-11-04 11:10:45 -0400

    fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,12 @@
 <script>
 	import { page } from '$app/stores';
 	import ArticleList from '$lib/ArticleList/index.svelte';
-	import Pagination from '$lib/Pagination.svelte';
+	import Pagination from './Pagination.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
 
-	$: p = +$page.url.searchParams.get('page') ?? 1;
+	$: p = +($page.url.searchParams.get('page') ?? '1');
 	$: tag = $page.url.searchParams.get('tag');
 	$: tab = $page.url.searchParams.get('tab') ?? 'all';
 	$: page_link_base = tag ? `tag=${tag}` : `tab=${tab}`;
@@ -37,7 +37,7 @@
 							</a>
 						</li>
 
-						{#if data}
+						{#if data.user}
 							<li class="nav-item">
 								<a href="/?tab=feed" class="nav-link" class:active={tab === 'feed'}>Your Feed</a>
 							</li>
