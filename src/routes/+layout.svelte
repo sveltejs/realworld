25afc89b15ad1ffe5162e64135fcddb2d06d9ae5
Scott Wu <sw@scottwu.ca>
2025-11-03 12:51:32 +0800

    use vite to import favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,15 @@
 <script>
 	import { navigating } from '$app/state';
+	import favicon from '$lib/assets/favicon.ico';
 	import Nav from './Nav.svelte';
 	import PreloadingIndicator from './PreloadingIndicator.svelte';
 
 	const { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
 
 {#if navigating}
 	<PreloadingIndicator />
